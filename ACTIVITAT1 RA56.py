9b3ab253b4f427ba54503f6855bb2468b33bac9e
PauMas7406¨ <2024_pau.mas@iticbcn.cat>
2025-04-04 12:27:37 +0200

    a

--- ACTIVITAT1 RA56.py
+++ ACTIVITAT1 RA56.py
@@ -1,11 +1,11 @@
 #Pau Mas Sales
-#28/3/2025
-#Archiu plantilla diferents programes Python
+#4/4/2025
+#Activitat 1 RA56
 
 
 #LLibreries 
 import psycopg
-
+#definicio que ens permet conetar-nos al PostgresSQL
 def connection():
 	connexio="""
 		dbname=chinook_v2
@@ -22,11 +22,20 @@ def connection():
 
 #Definico que em mostra per consola les opcions del menu
 def printar_menu():
-    print("")
+    conn=connection()
+    print("Menú Principal")
+    print("1 - Consultar tots els artistes")
+    print("2 - Consultar artistes pel seu nom")
+    print("3 - Consultar els 5 primers àlbums pel nom de l'artista")
+    print("4 - Afegir un artista")
+    print("5 - Modificar el nom d'un artista")
+    print("6 - Borrar un artista")
+    print("7 - Sortir")
+    opcio=input("Selecciona una opció: ")
 
 
 
-#Definico que demana l'ocio a l'usuari i executa la definicio corresponent
+#Definico que demana l'opcio a l'usuari i executa la definicio corresponent
 def escollir_opcio():
     opcio=int(input("Escull opcio"))
     if opcio==1 :
@@ -51,5 +60,8 @@ def main():
 	menu()
 
 
-if__name__=="__main__":
-	main(nomBase)
+
+
+#PER A EXECUTAR PRIMERAMENT LA FUNCIÓ menu
+if __name__ == "__main__":
+    menu()
